c2d5816a0e14523875324784730f477551fcf8ff
Horsend <yagoyagofaria@gmail.com>
2020-09-14 17:05:11 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  // VARIAVEIS
+  final _tPreco = TextEditingController();
+  final _tPessoas = TextEditingController();
+  var _infoText = "Sera que ta caro?";
+  var _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Racha conta"),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh),
+              onPressed: _resetFields)
+        ],
+      ),
+      body: _body(),
+    );
+  }
+
+  // PROCEDIMENTO PARA LIMPAR OS CAMPOS
+  void _resetFields(){
+    _tPreco.text = "";
+    _tPessoas.text = "";
+    setState(() {
+      _infoText = "Será que ta caro?";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  _body() {
+    return SingleChildScrollView(
+        padding: EdgeInsets.all(15.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _editText("Preço", _tPreco),
+              _editText("Numero de pessoas", _tPessoas),
+              _buttonCalcular(),
+              _textInfo(),
+            ],
+          ),
+        ));
+  }
+
+  // Widget text
+  _editText(String field, TextEditingController controller) {
+    return TextFormField(
+      controller: controller,
+      validator: (s) => _validate(s, field),
+      keyboardType: TextInputType.number,
+      style: TextStyle(
+        fontSize: 22,
+        color: Colors.blue,
+      ),
+      decoration: InputDecoration(
+        labelText: field,
+        labelStyle: TextStyle(
+          fontSize: 22,
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+
+  // PROCEDIMENTO PARA VALIDAR OS CAMPOS
+  String _validate(String text, String field) {
+    if (text.isEmpty) {
+      return "Digite $field";
+    }
+    return null;
+  }
+
+  // Widget button
+  _buttonCalcular() {
+    return Container(
+      margin: EdgeInsets.only(top: 10.0, bottom: 20),
+      height: 45,
+      child: RaisedButton(
+        color: Colors.blue,
+        child:
+        Text(
+          "Calcular",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+        onPressed: () {
+          if(_formKey.currentState.validate()){
+            _calculate();
+          }
+        },
+      ),
+    );
+  }
+
+  // PROCEDIMENTO PARA CALCULAR O IMC
+  void _calculate(){
+    setState(() {
+      double peso = double.parse(_tPreco.text);
+      double altura = double.parse(_tPessoas.text);
+      double imc = peso / altura;
+      String imcStr = imc.toStringAsPrecision(4);
+      if(imc <= 50) {
+        _infoText = "RS ($imcStr) mto bom";
+      }
+      if(imc > 50) {
+        _infoText = "RS ($imcStr) da pra pagar vai";
+      }
+      if(imc >= 100){
+        _infoText= "Poxa R ($imcStr), ta caro viu";
+      }
+    });
+  }
+
+  // // Widget text
+  _textInfo() {
+    return Text(
+      _infoText,
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.blue, fontSize: 25.0),
+    );
+  }
+}
